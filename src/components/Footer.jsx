c722c99858eef3b2f2b6d9a37890e1df78e282b4
Milo Juarez <ermilo98go@gmail.com>
2022-03-19 12:00:51 -0600

    Added Footer and styled the Layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "@styles/Footer.scss";
+
+const Footer = () => {
+    return (
+        <div className='Footer'>
+            <p className='Footer__copyright'>Footer</p>
+        </div>
+    );
+};
+
+export default Footer;
--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Header from "@components/Header";
 import "@styles/Layout.scss";
+import Footer from "../components/Footer";
 
 const Layout = ({ children }) => {
     return (
         <div className='Layout'>
             <Header />
-            <div>{children}</div>
+            <div className='Layout__pages'>{children}</div>
+            <Footer />
         </div>
     );
 };
