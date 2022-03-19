a7454ec97cb83c407d279fa4a3e73dbaecaa09a8
Milo Juarez <ermilo98go@gmail.com>
2022-03-19 02:27:34 -0600

    Created Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "@styles/Navbar.scss";
+import NavItem from "@components/NavItem";
+
+const Navbar = () => {
+    return (
+        <nav className='Navbar'>
+            <NavItem label={"INICIO"} active={false} url={"/#inicio"} />
+            <NavItem label={"ESCUELAS"} active={true} url={"/#escuelas"} />
+            <NavItem label={"REPORTES"} active={false} url={"/#reportes"} />
+            <NavItem
+                label={"HABILITAR CAPTURA"}
+                active={false}
+                url={"/#captura"}
+            />
+        </nav>
+    );
+};
+
+export default Navbar;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "@styles/global.css";
-import NavItem from "../components/NavItem";
+import Navbar from "@components/Navbar";
 
 const App = () => {
     return (
         <React.Fragment>
             <h1>BUILDING...</h1>
-            <NavItem label={"Escuelas"} active={true} url={"/#escuelas"} />
-            <NavItem label={"Inicio"} active={false} url={"/#inicio"} />
+            <Navbar />
         </React.Fragment>
     );
 };
