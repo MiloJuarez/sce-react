2b9f81f22c5378492ce5a0f7966c3c53667024e7
Milo Juarez <ermilo98go@gmail.com>
2022-03-23 15:28:51 -0600

    Styled schools' container

--- src/containers/SchoolTable.jsx
+++ src/containers/SchoolTable.jsx
@@ -10,6 +10,7 @@ import ActionButtonEdit from "@components/ActionButtonEdit";
 import ActionButtonsContainer from "@components/ActionButtonsContainer";
 import TblRowCell from "@components/TblRowCell";
 import TblRow from "@components/TblRow";
+import "@styles/SchoolTable.scss";
 
 const SchoolTable = () => {
     let schools = getSchools();
@@ -21,24 +22,26 @@ const SchoolTable = () => {
                 <TblColumnHeader label={"Ubicacion"} />
                 <TblColumnHeader label={"Acciones"} />
             </TblHeader>
-            {schools.map((school) => {
-                return (
-                    <TblRow
-                        cells={2}
-                        index={school.id}
-                        key={`schoolItem-${school.id}`}>
-                        <TblRowCell value={school.name} />
-                        <TblRowCell value={school.location} />
-                        <TblRowCell>
-                            <ActionButtonsContainer>
-                                <ActionButtonSelect model={"Escuela"} />
-                                <ActionButtonDetails model={"Escuela"} />
-                                <ActionButtonEdit model={"Escuela"} />
-                            </ActionButtonsContainer>
-                        </TblRowCell>
-                    </TblRow>
-                );
-            })}
+            <div className='SchoolTable__body'>
+                {schools.map((school) => {
+                    return (
+                        <TblRow
+                            cells={2}
+                            index={school.id}
+                            key={`schoolItem-${school.id}`}>
+                            <TblRowCell value={school.name} />
+                            <TblRowCell value={school.location} />
+                            <TblRowCell>
+                                <ActionButtonsContainer>
+                                    <ActionButtonSelect model={"Escuela"} />
+                                    <ActionButtonDetails model={"Escuela"} />
+                                    <ActionButtonEdit model={"Escuela"} />
+                                </ActionButtonsContainer>
+                            </TblRowCell>
+                        </TblRow>
+                    );
+                })}
+            </div>
         </div>
     );
 };
