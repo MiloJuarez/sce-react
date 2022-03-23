cc4840491a8753ce6b6b7074c81287e315bbc333
Milo Juarez <ermilo98go@gmail.com>
2022-03-23 12:55:22 -0600

    Moved title property to its parent

--- src/components/BtnArrowBack.jsx
+++ src/components/BtnArrowBack.jsx
@@ -6,8 +6,8 @@ import "@styles/BtnArrowBack.scss";
 
 const BtnArrowBack = ({ goBack }) => {
     return (
-        <div className='BtnArrowBack' onClick={goBack}>
-            <FaArrowLeft className='BtnArrowBack__icon' title='Regresar' />
+        <div className='BtnArrowBack' onClick={goBack} title='Regresar'>
+            <FaArrowLeft className='BtnArrowBack__icon' />
         </div>
     );
 };
