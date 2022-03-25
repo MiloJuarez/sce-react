4aa8ab36e687e4ad14e82ae5af299d266003414b
Milo Juarez <ermilo98go@gmail.com>
2022-03-25 01:01:25 -0600

    Added inputs wrapper

---
+++ src/components/FormControl.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "@styles/FormControl.scss";
+
+const FormControl = ({ children }) => {
+    return <div className='FormControl'>{children}</div>;
+};
+
+export default FormControl;
